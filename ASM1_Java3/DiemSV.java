52fdeb89cf7b25da0856261e735b55598e7064a5
Hien Pham <thehienpv@gmail.com>
2020-07-18 10:56:42 +0700

    ASM_Java3

---
+++ ASM1_Java3/DiemSV.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ASM;
+
+/**
+ *
+ * @author Admin
+ */
+public class DiemSV {
+    int ID;
+    String maSV;
+    String hoTen;
+    int TiengAnh;
+    int TinHoc;
+    int GDTC;
+
+    public DiemSV(int ID, String maSV, String hoTen, int TiengAnh, int TinHoc, int GDTC) {
+        this.ID = ID;
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.TiengAnh = TiengAnh;
+        this.TinHoc = TinHoc;
+        this.GDTC = GDTC;
+    }
+    
+}
